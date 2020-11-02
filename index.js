5ab43e47212351252725a8dc29b88baf57606716
Viktoryia <kulinkovichva@gmail.com>
2020-11-02 12:42:09 +0300

    add wilders

---
+++ index.js
@@ -0,0 +1,6 @@
+const chalk = require('chalk');
+ 
+console.log(chalk.blue('Hello Teiko!'));
+console.log(chalk.red('Hello Jane!'));
+console.log(chalk.green('Hello Carlos!'));
+console.log(chalk.cyan('Hello Marc!'));
